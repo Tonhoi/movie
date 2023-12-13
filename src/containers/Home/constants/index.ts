562c8556633d19b943ea3bb392ab473b770f39ea
ton that hoi <tonthathoi.090302@gmail.com>
2023-12-13 19:20:06 +0700

    update code

--- src/containers/Home/constants/index.ts
+++ src/containers/Home/constants/index.ts
@@ -1,4 +1,5 @@
 export const objectSeo = {
     seo_title: "Dữ liệu phim vietsub miễn phí mới nhất. Cập nhật nhanh chóng, chất lượng cao và không quảng cáo",
     seo_description: "Website cung cấp phim miễn phí nhanh chất lượng cao. Phim online VietSub, Thuyết minh, lồng tiếng chất lượng Full HD. Nguồn phim vietsub chất lượng cao cập nhật nhanh nhất",
+    og_image: require("@/assets/images/default_image.jpg")
 }
