1e970fdde6f0d497e73470dfb0d9855b7555e8e6
ton that hoi <tonthathoi.090302@gmail.com>
2023-12-14 09:57:52 +0700

    update code

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -12,7 +12,7 @@ type SEOProps = {
 };
 
 const SEO = (props: SEOProps) => {
-  const { seo_title, seo_description, defaultNextSeo, og_url, og_image = require("../../public/default_image.jpg") } = props;
+  const { seo_title, seo_description, defaultNextSeo, og_url, og_image } = props;
 
   return (
     <NextSeo
@@ -27,7 +27,7 @@ const SEO = (props: SEOProps) => {
         type: "website",
         images: [
           {
-            url: og_image,
+            url: og_image ?? require('@/assets/images/default_og_image.jpg').default.src,
             alt: seo_title,
             type: "image/jpeg",
           },
--- src/containers/Home/constants/index.ts
+++ src/containers/Home/constants/index.ts
@@ -1,5 +1,4 @@
 export const objectSeo = {
     seo_title: "Dữ liệu phim vietsub miễn phí mới nhất. Cập nhật nhanh chóng, chất lượng cao và không quảng cáo",
     seo_description: "Website cung cấp phim miễn phí nhanh chất lượng cao. Phim online VietSub, Thuyết minh, lồng tiếng chất lượng Full HD. Nguồn phim vietsub chất lượng cao cập nhật nhanh nhất",
-    og_image: ""
 }
